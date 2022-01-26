e812c069e8868769956ff4303fef2ccd77712701
altj <denis.kha@okwork.io>
2022-01-27 01:53:31 +0300

    check variables in site

--- app/routes/careers/$careerId.tsx
+++ app/routes/careers/$careerId.tsx
@@ -15,12 +15,7 @@ export const loader: LoaderFunction = async ({ params, context}) => {
     envData: null
   };
   const {
-    request, // same as existing Worker API
     env, // same as existing Worker API
-    params, // if filename includes [id] or [[path]]
-    waitUntil, // same as ctx.waitUntil in existing Worker API
-    next, // used for middleware or to fetch assets
-    data, // arbitrary space for passing data between middlewares
   } = context;
   data1.tableData = await getTable();
   data1.careerData = await getVacancy(params.careerId);

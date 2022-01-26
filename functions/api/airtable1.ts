c5cae2a08e3aa831892998bca8b03468fb597b71
altj <denis.kha@okwork.io>
2022-01-27 02:58:32 +0300

    check variables in site

--- functions/api/airtable1.ts
+++ functions/api/airtable1.ts
@@ -1,14 +1,14 @@
-export async function onRequest(context) {
+export const onRequest = async context => {
     // Contents of context object
     const {
         env, // same as existing Worker API
     } = context;
 
-    const envData = new Response(env);
-    const response = await fetch(`https://api.airtable.com/v0/${envData.BASE_ID}/${envData.VACANCY_TABLE}?maxRecords=99&view=Grid%20view`, {
+    const {AIRTABLE_API_KEY, BASE_ID, VACANCY_TABLE} = new Response(env);
+    const response = await fetch(`https://api.airtable.com/v0/${BASE_ID}/${VACANCY_TABLE}?maxRecords=99&view=Grid%20view`, {
         headers: {
-            Authorization: `Bearer ${envData.AIRTABLE_API_KEY}`,
+            Authorization: `Bearer ${AIRTABLE_API_KEY}`,
         },
     })
     return await response.json();
-}
+};

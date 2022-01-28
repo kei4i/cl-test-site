629ce55e38a74e2d92e4ab1f5b25bf3051ca764e
altj <denis.kha@okwork.io>
2022-01-28 03:08:47 +0300

    add test function

--- app/routes/privacy.tsx
+++ app/routes/privacy.tsx
@@ -1,19 +1,22 @@
 import type {MetaFunction, ActionFunction} from "remix";
 import {LoaderFunction, useLoaderData} from "remix";
 import VacanciesList from "~/components/vacancies";
-import {getTable} from "~/api/airtable";
+import {onRequest} from "../../functions/api/airtable/getTable1";
 export const meta: MetaFunction = () => {
   return {
     title: "Cadolabs - privacy policy",
   }
 };
 
-export const loader: LoaderFunction = async () => {
-  return getTable();
-};
+export const loader: LoaderFunction = async (context) => {
+  const response = onRequest(context);
+  const data = await response;
+  return data;
+}
+
 
 export default function Privacy() {
-  const vacanciesList = useLoaderData().records;
+  const vacanciesList = JSON.parse(useLoaderData()).records;
   return (
       <div>
         <section className="content">

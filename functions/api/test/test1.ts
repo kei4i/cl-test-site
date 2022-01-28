a4f3f31702d9db485b40c358466b0d4af4b76e51
altj <denis.kha@okwork.io>
2022-01-28 18:22:08 +0300

    add test function

--- functions/api/test/test1.ts
+++ functions/api/test/test1.ts
@@ -1,8 +1,8 @@
-import {jsonResponse} from "../utils/jsonResponse";
+import {jsonResponse} from "../../utils/jsonResponse";
 
 export const onRequestPost: PagesFunction<{ PAGES: KVNamespace }> = async ({request, env}) => {
         const { accessAud } = await request.json();
-        const { apiToken, accountId } = (await env.IMAGES.get(
+        const { apiToken, accountId } = (await env.PAGES.get(
             "AIRTABLE_API_KEY1",
             "BASE_ID1"
         )) as Setup;

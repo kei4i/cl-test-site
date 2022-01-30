24dd1f4e9fbc7fe86f2a47ab680ed1250a3a083a
altj <denis.kha@okwork.io>
2022-01-30 03:56:35 +0300

    add base url in fetch

--- functions/api/airtable/getTable.ts
+++ functions/api/airtable/getTable.ts
@@ -4,9 +4,11 @@ export async function onRequest(context) {
     } = context;
 
     // const {AIRTABLE_API_KEY, BASE_ID, VACANCY_TABLE} = new Response(env);
-    const res = await fetch(`https://api.airtable.com/v0/${env.BASE_ID}/${env.VACANCY_TABLE}?maxRecords=99&view=Grid%20view`, {
-        headers: {
-            Authorization: `Bearer ${env.AIRTABLE_API_KEY}`,
+    // const res = await fetch(`https://api.airtable.com/v0/${env.BASE_ID}/${env.VACANCY_TABLE}?maxRecords=99&view=Grid%20view`, {
+    const res = await fetch('https://api.airtable.com/v0/app2oHOQICP1Dve1H/vacancies?maxRecords=3&view=Grid%20view', {
+    headers: {
+            // Authorization: `Bearer ${env.AIRTABLE_API_KEY}`,
+            Authorization: `Bearer keyFVAbr7P6w5rzBy`,
         },
     })
     const data = await res.json();

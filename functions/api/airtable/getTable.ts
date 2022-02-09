a204d114dac6932570676e2f2698fd82c788943c
altj <denis.kha@okwork.io>
2022-02-09 19:31:46 +0300

    move load career data to separate route edit all pages for new logic

--- functions/api/airtable/getTable.ts
+++ functions/api/airtable/getTable.ts
@@ -3,9 +3,10 @@ export async function onRequest(context) {
         env, // same as existing Worker API
     } = context;
 
-    const res = await fetch(`https://api.airtable.com/v0/${env.BASE_ID}/${env.VACANCY_TABLE}?maxRecords=99&view=Grid%20view`, {
+    // const res = await fetch(`https://api.airtable.com/v0/${env.BASE_ID}/${env.VACANCY_TABLE}?maxRecords=99&view=Grid%20view`, {
+    const res = await fetch(`https://api.airtable.com/v0/app2oHOQICP1Dve1H/vacancies?maxRecords=99&view=Grid%20view`, {
     headers: {
-            Authorization: `Bearer ${env.AIRTABLE_API_KEY}`,
+            Authorization: `Bearer keyFVAbr7P6w5rzBy`,
         },
     })
     const data = await res.json();

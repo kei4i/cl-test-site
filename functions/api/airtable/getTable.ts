960a9757c4b6d1eff7b7d4ca3fd208c5350721ad
altj <denis.kha@okwork.io>
2022-02-10 12:10:18 +0300

    change api

--- functions/api/airtable/getTable.ts
+++ functions/api/airtable/getTable.ts
@@ -3,10 +3,9 @@ export async function onRequest(context) {
         env, // same as existing Worker API
     } = context;
 
-    // const res = await fetch(`https://api.airtable.com/v0/${env.BASE_ID}/${env.VACANCY_TABLE}?maxRecords=99&view=Grid%20view`, {
-    const res = await fetch(`https://api.airtable.com/v0/app2oHOQICP1Dve1H/vacancies?maxRecords=99&view=Grid%20view`, {
+    const res = await fetch(`https://api.airtable.com/v0/${env.BASE_ID}/${env.VACANCY_TABLE}?maxRecords=99&view=Grid%20view`, {
     headers: {
-            Authorization: `Bearer keyFVAbr7P6w5rzBy`,
+            Authorization: `Bearer ${env.AIRTABLE_API_KEY}`,
         },
     })
     const data = await res.json();
